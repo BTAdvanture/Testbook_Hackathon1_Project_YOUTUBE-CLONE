a3aaa1cb31a0067160f5c2a08e752481b63a4c9a
Harshal Raju Chimankar <110530089+BTAdvanture@users.noreply.github.com>
2022-10-19 21:11:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#menu');
+console.log(menu);
+const sidebar = document.querySelector('.sidebar');
+menu.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar');
+});
